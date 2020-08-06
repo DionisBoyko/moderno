6e8bdf4e410724f83b4789c835679a84551a40e6
DionisBoyko <bojkodenis@gmail.com>
2020-08-06 11:18:51 +0300

    added product-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,16 @@
 $(function () {
 
    $(".rate-star").rateYo({
-      rating: 5
+      rating: 5,
+      readOnly: true,
+      starWidth: "12px"
+   });
+
+   $('.product-slider__inner').slick({
+      dots: true,
+      arrows: false,
+      slidesToShow: 4,
+      slidesToScroll: 4
    });
 
    var mixer = mixitup('.products__inner-box');
